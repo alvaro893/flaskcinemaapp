793e7196bff113b4a0d5f70afbc1e7c0eb000173
alvaro893 <alvarobolro@gmail.com>
2016-03-21 23:05:53 +0200

    add virtual env and fix whitespace

--- FlaskWebProject/services/finnkino.py
+++ FlaskWebProject/services/finnkino.py
@@ -9,7 +9,6 @@ class FinnKinoXML(object):
         'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0",
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     }
-	
     def get_movies_for_area(self, area_code):
         request_url = "{}?area={}".format(self.schedule_url,
                         area_code)
