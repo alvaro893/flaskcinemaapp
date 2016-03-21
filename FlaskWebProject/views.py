e2ee50cfc01eb34e4a820d06e810d5de0179a9a0
alvaro893 <alvarobolro@gmail.com>
2016-03-21 03:43:23 +0200

    Revert "add cinema project"
    This reverts commit 28f1f146b95a401ec4ee2ef058ef1274cc972f02.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,28 +5,8 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
-from services.finnkino import FinnKinoXML
-
-finnkino = FinnKinoXML()
-
 
 @app.route('/')
-def main_page():
-    months = ["January", "February", "March", "April"]
-    my_dict = {"hello":"it is a greeting", 'mug':"it is a thing to hold coffee", "plate":"it is an object that contens food"}
-    return render_template('mainpage.html', months = months, my_dict = my_dict)
-
-@app.route('/movies')
-def movies_page():
-    movies = finnkino.get_movies_for_area(area_num)
-    return render_template('maincinemapage.html', movies = movies)
-    
-@app.route('/movies/<area>')
-def movies(area):
-    movies = finnkino.get_movies_for_area(area)
-    areas = finnkino.get_areas()
-    return render_template('maincinemapage.html', movies = movies, areas = areas)
-
 @app.route('/home')
 def home():
     """Renders the home page."""
